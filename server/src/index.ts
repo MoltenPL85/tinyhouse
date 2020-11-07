6e24d057a80588fc163fa4e33992c7df18887e46
MoltenPL85 <bugaev.maxim.85@gmail.com>
2020-11-07 23:41:59 +0500

    Change REST to GraphQL.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,12 @@
 import express from 'express';
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './graphql';
 
 const app = express();
 const port = 9000;
 
-app.use(express.json());
-
-app.get('/listings', (_req, res) => res.send(listings));
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send('Failed to delete listing');
-});
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
 
